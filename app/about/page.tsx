f9ed4a7d2d62278269dbcd3303ba795ca35a31f7
Mitchell <mitchbowell12@gmail.com>
2025-08-06 16:09:26 +1000

    added start for about page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <br></br>
+    <h2>mini mini title</h2>
+    <body>
+      exapmle of lots of super cool work <br></br>
+      I love coding so so much!!!!  <br></br>
+    </body>
+    </div>
+
+
+  );
+}
