df1d906ca85329a157366d1662fdbede778a9cc0
Mitchell <mitchbowell12@gmail.com>
2025-08-07 15:43:33 +1000

    updated Menu to become a proper Burger menu

--- app/Hamburger_Menu/page.tsx
+++ app/Hamburger_Menu/page.tsx
@@ -1,6 +1,6 @@
 export default function HamburgerMenuPage() {
   return (
-    <div><h1 style={{ textAlign: 'center', marginTop: '10px', marginBottom: '20px' }}>Menu</h1>
+    <div><h1 style={{ textAlign: 'center', marginTop: '10px', marginBottom: '20px' }}><b>Hamburger Menu</b></h1>
     <div
       style={{
         display: 'flex',
@@ -16,37 +16,37 @@ export default function HamburgerMenuPage() {
         {/* Specials */}
         <h2><u>Specials</u></h2>
         <p>
-          hot honey chicken burger - $19.99<br />
-          hot honey chicken bites - $18.00<br />
+          Hot Honey Chicken Burger - $19.99<br />
+          Hot Honey Chicken Bites - $18.00<br />
         </p>
         <br />
         {/* Burgers */}
-        <h2><u>Burgers</u></h2>
+        <h2><u>Beef Burgers</u></h2>
         <p>
-          crunchy chicken burger - $16.90<br />
-          micro chicken burger - $14.50<br />
-          spicy beef burger - $16.50 <br />
-          BBQ beef burger - $15.00<br />
+          Plain Beef Burger - $16.90<br />
+          Micro Beeg Burger - $14.50<br />
+          Spicy Beef Burger - $16.50 <br />
+          BBQ Beef Burger - $15.00<br />
         </p>
       </div>
 
       {/* Center Half of Menu */}
       <div style={{ flex: 1, textAlign: 'left', padding: '0 40px', marginTop: '2%' }}>
         {/* Kebab */}
-        <h2><u>Kebab</u></h2>
+        <h2><u>Chicken Burger</u></h2>
         <p>
-          crunchy chicken kebab - $16.90<br />
-          BBQ beef kebab - $14.50<br />
-          Mixed kebab - $16.50<br />
+          Crunchy Chicken Burger - $16.90<br />
+          BBQ Chicken Burger - $14.50<br />
+          Spicey Chicken Burger - $16.50<br />
         </p>
         <br />
         {/* Sides */}
         <h2><u>Sides</u></h2>
         <p>
           large Chips + 1.1L drink - $9.99<br />
-          Small chips - $3.50<br />
-          Large chips -  $5.99<br />
-          1.1L drink - $6.00<br />
+          Small Chips - $3.50<br />
+          Large Chips -  $5.99<br />
+          1.1L Drink - $6.00<br />
         </p>
       </div>
 
